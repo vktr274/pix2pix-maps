8f5fee9fbd6faff3a7a994f3be2a99624540548f
Viktor Modroczký <v.modroczky@gmail.com>
2023-04-25 23:38:45 +0200

    training works

--- src/pix2pix.py
+++ src/pix2pix.py
@@ -1,8 +1,11 @@
 from typing import Tuple, Union
+import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow.keras.losses import BinaryCrossentropy, MeanAbsoluteError
+from tensorflow.keras.initializers import RandomNormal
 from tensorflow.keras import Model, Sequential, Input
 from tensorflow.keras.utils import plot_model
+from tensorflow.keras.optimizers import Optimizer
 from tensorflow.keras.layers import (
     Conv2D,
     Conv2DTranspose,
@@ -17,7 +20,7 @@ bce = BinaryCrossentropy(from_logits=True)
 l1 = MeanAbsoluteError()
 
 
-def GeneratorLoss(
+def g_loss(
     l1_lambda: float,
     fake_G: tf.Tensor,
     fake_D: tf.Tensor,
@@ -27,7 +30,7 @@ def GeneratorLoss(
     return fake_loss + l1_lambda * l1(fake_G, y)
 
 
-def DiscriminatorLoss(real_D: tf.Tensor, fake_D: tf.Tensor) -> tf.Tensor:
+def d_loss(real_D: tf.Tensor, fake_D: tf.Tensor) -> tf.Tensor:
     real_loss = bce(tf.ones_like(real_D), real_D)
     fake_loss = bce(tf.zeros_like(fake_D), fake_D)
     return real_loss + fake_loss
@@ -42,7 +45,15 @@ def downsampling_block(
 ) -> Sequential:
     down = Sequential()
 
-    down.add(Conv2D(filters, kernel_size=kernel_size, strides=strides, padding="same"))
+    down.add(
+        Conv2D(
+            filters,
+            kernel_size=kernel_size,
+            strides=strides,
+            padding="same",
+            kernel_initializer=RandomNormal(mean=0.0, stddev=0.02),
+        )
+    )
     if use_batchnorm:
         down.add(BatchNormalization())
     if dropout:
@@ -63,7 +74,11 @@ def upsampling_block(
 
     up.add(
         Conv2DTranspose(
-            filters, kernel_size=kernel_size, strides=strides, padding="same"
+            filters,
+            kernel_size=kernel_size,
+            strides=strides,
+            padding="same",
+            kernel_initializer=RandomNormal(mean=0.0, stddev=0.02),
         )
     )
     if use_batchnorm:
@@ -102,7 +117,12 @@ def UNet(
     ]
 
     last = Conv2DTranspose(
-        3, kernel_size=(4, 4), strides=(2, 2), activation="tanh", padding="same"
+        3,
+        kernel_size=(4, 4),
+        strides=(2, 2),
+        activation="tanh",
+        padding="same",
+        kernel_initializer=RandomNormal(mean=0.0, stddev=0.02),
     )
 
     out = inputs
@@ -145,9 +165,95 @@ def PatchGAN(
         out = downsampling_block(512, kernel_size=kernel_size)(out)
         out = downsampling_block(512, kernel_size=kernel_size)(out)
 
-    out = Conv2D(1, kernel_size=kernel_size, padding="same", activation="sigmoid")(out)
+    out = Conv2D(
+        1,
+        kernel_size=kernel_size,
+        padding="same",
+        activation="sigmoid",
+        kernel_initializer=RandomNormal(mean=0.0, stddev=0.02),
+    )(out)
 
     return Model(inputs=[inputs, targets], outputs=out)
+
+
+@tf.function
+def train_step(
+    generator: Model,
+    discriminator: Model,
+    generator_optimizer: Optimizer,
+    discriminator_optimizer: Optimizer,
+    l1_lambda: float,
+    input_image: tf.Tensor,
+    target: tf.Tensor,
+) -> Tuple[tf.Tensor, tf.Tensor]:
+    with tf.GradientTape() as g_tape, tf.GradientTape() as d_tape:
+        fake_G = generator(input_image, training=True)
+
+        real_D = discriminator([input_image, target], training=True)
+        fake_D = discriminator([input_image, fake_G], training=True)
+
+        gen_loss = g_loss(l1_lambda, fake_G, fake_D, target)
+        disc_loss = d_loss(real_D, fake_D)
+
+    generator_gradients = g_tape.gradient(gen_loss, generator.trainable_variables)
+    discriminator_gradients = d_tape.gradient(
+        disc_loss, discriminator.trainable_variables
+    )
+
+    generator_optimizer.apply_gradients(
+        zip(generator_gradients, generator.trainable_variables)
+    )
+    discriminator_optimizer.apply_gradients(
+        zip(discriminator_gradients, discriminator.trainable_variables)
+    )
+
+    return gen_loss, disc_loss
+
+
+def generate_image(generator: Model, example_input, example_target):
+    prediction = generator(example_input, training=True)
+    plt.figure(figsize=(15, 15))
+
+    display_list = [example_input[0], example_target[0], prediction[0]]
+    title = ["Input Image", "Ground Truth", "Predicted Image"]
+
+    for i in range(3):
+        plt.subplot(1, 3, i + 1)
+        plt.title(title[i])
+        plt.imshow(display_list[i] * 0.5 + 0.5)
+        plt.axis("off")
+    plt.show()
+
+
+def fit(
+    train_data,
+    val_data,
+    epochs: int,
+    generator: Model,
+    discriminator: Model,
+    generator_optimizer: Optimizer,
+    discriminator_optimizer: Optimizer,
+    l1_lambda: float,
+) -> None:
+    for epoch in range(epochs):
+        example_input, example_target = next(iter(val_data.take(1)))
+        for step, (input_patches, target_patches) in enumerate(train_data):
+            gen_loss, disc_loss = train_step(
+                generator,
+                discriminator,
+                generator_optimizer,
+                discriminator_optimizer,
+                l1_lambda,
+                input_patches,
+                target_patches,
+            )
+
+            if step % 100 == 0:
+                print(
+                    f"Epoch: {epoch + 1}, Step: {step}, Gen Loss: {gen_loss}, Disc Loss: {disc_loss}",
+                    flush=True,
+                )
+        generate_image(generator, example_input, example_target)
 
 
 if __name__ == "__main__":
